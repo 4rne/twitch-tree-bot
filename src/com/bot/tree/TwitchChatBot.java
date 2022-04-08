49736c043d5f3fe1e74a62b042224912deb865b3
ThePseud0o <maps.arne@googlemail.com>
2022-04-08 19:22:23 +0200

    bot emoji in weather answer

--- src/com/bot/tree/TwitchChatBot.java
+++ src/com/bot/tree/TwitchChatBot.java
@@ -112,7 +112,7 @@ public class TwitchChatBot extends PircBot {
 				sendMessage(channel, "ArbPripps You cannot translate glorious Pripps Blå ArbPripps ");
 			}
 			if(message.toLowerCase().startsWith("!weather") && !sender.equalsIgnoreCase("Pseud0obot")) {
-				sendMessage(channel, weather.toReadableString());
+				sendMessage(channel, "🤖" + weather.toReadableString());
 			}
 		}
 	}
