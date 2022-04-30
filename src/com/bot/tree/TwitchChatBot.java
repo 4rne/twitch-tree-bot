a4f51af9b10c6af676637cbc9f0d25039577119c
ThePseud0o <maps.arne@googlemail.com>
2022-04-30 23:29:12 +0200

    fix team size for estimate

--- src/com/bot/tree/TwitchChatBot.java
+++ src/com/bot/tree/TwitchChatBot.java
@@ -143,7 +143,7 @@ public class TwitchChatBot extends PircBot {
 		ArrayList<String> fullTeam = new ArrayList<String> (Arrays.asList("Stepan", "Vektor", "JP", "CrazyGroundie", "Bjurn", "Karlous"));
 		ArrayList<String> prices = new ArrayList<String> (Arrays.asList("over 9000", "too much", "a kidney and 3", "1", (new Random().nextInt(5999) + 4000) + "", "1337", "69", "3.14159", "666", "42", "420"));
 		ArrayList<String> jobTeam = new ArrayList<String>();
-		int teamSize = new Random().nextInt(fullTeam.size() - 1) + 1;
+		int teamSize = new Random().nextInt(fullTeam.size() - 2) + 2;
 		for(int i = 0; i < teamSize; i++) {
 			int memberPosition = new Random().nextInt(fullTeam.size());
 			jobTeam.add(fullTeam.get(memberPosition));
