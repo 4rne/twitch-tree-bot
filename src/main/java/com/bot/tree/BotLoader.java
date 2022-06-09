b660c9ff6cc958eb8dc77d457e92736160de498b
ThePseud0o <maps.arne@googlemail.com>
2022-06-09 12:15:14 +0200

    improve param loader

--- src/main/java/com/bot/tree/BotLoader.java
+++ src/main/java/com/bot/tree/BotLoader.java
@@ -1,20 +1,28 @@
 package com.bot.tree;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class BotLoader {
     public static void main(String[] args) {
         String params[] = new String[] {"TWITCH_API_TOKEN", "BOT_NAME", "JOIN_CHANNELS", "TWITCH_CLIENT_ID", "TWITCH_CLIENT_SECRET", "OPENWEATHERMAP_API_TOKEN", "TWITCH_ACCESS_TOKEN", "EVENT_CHANNEL_ID"};
         HashMap<String, String> config_options = new HashMap<String, String>();
+        ArrayList<String> missingParams = new ArrayList<String>();
 
         for (String param : params) {
             String value = System.getenv().get(param);
             if(value == null) {
-                System.err.println("no " + param + " provided");
-                System.exit(1);
+                missingParams.add(param);
             } else {
                 config_options.put(param, value);
             }
+        }
+
+        if(missingParams.size() > 0) {
+            for (String param : missingParams) {
+                System.err.println("no " + param + " provided");
+            }
+            System.exit(1);
         }
 
         TwitchChatBot bot = new TwitchChatBot(config_options);
